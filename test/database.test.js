37edaef292bc5fc88c869dd70dee50f123053d3c
linpeng109 <linpeng109@aliyun.com>
2016-05-06 10:24:34 +0800

    10:24

--- test/database.test.js
+++ test/database.test.js
@@ -2,15 +2,26 @@
  * Created by Mars on 2016/5/5.
  */
 var database = require('../lib/database');
+var User = require('../lib/user')(database);
 var expect = require('chai').expect;
 
-describe('测试数据库连接', function () {
+describe('Test mongodb connecte', function () {
   before(function (done) {
     database.connection.on('connected', done);
   })
 
-  it('数据库是否已经连接？', function (done) {
+  it('mongodb is open ？', function (done) {
     var result = database.connection.readyState;
     expect(database.connection.readyState).to.be.equal(1) ? done() : done(false);
   })
-});
+  it('insert a user', function (done) {
+    var user = new User({
+      userName: 'abddd',
+      passWord: 'abddd'
+    });
+    User.insert(user, function (err, result) {
+      expect(err === null).to.be.ok ? done() : done(false);
+    });
+  });
+})
+;
